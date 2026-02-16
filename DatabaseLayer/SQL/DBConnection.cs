9c5bdfa6770c4857125954e401f959379bb46a73
arsaln195 <123059680+arsaln195@users.noreply.github.com>
2026-02-16 12:15:22 +0500

    Database Layer

---
+++ DatabaseLayer/SQL/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseLayer.SQL
+{
+    public partial class DatabaseDataContext : System.Data.Linq.DataContext
+    {
+        public DatabaseDataContext()
+            : base(System.Configuration.ConfigurationManager.ConnectionStrings["PocketDCRConnectionString"].ConnectionString)
+        {
+            OnCreated();
+        }
+    }
+}
